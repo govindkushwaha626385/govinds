3188af09ae6b36c832799574783b02282fe17f7d
GOVIND SINGH KUSHWAHA <127130035+govindkushwaha626385@users.noreply.github.com>
2023-10-27 23:40:51 +0530

    Update python.py

--- python.py
+++ python.py
@@ -7,7 +7,7 @@ def SieveOfEratosthenes(n, isPrime):
     for p in range(2,n+1):
         
         if (p*p<=n and isPrime[p] == True):
-            # Update all multiples of p
+          
             for i in range(p*2,n+1,p):
                 isPrime[i] = False
                 p += 1
@@ -15,19 +15,15 @@ def superPrimes(n):
      
     isPrime = [1 for i in range(n+1)]
     SieveOfEratosthenes(n, isPrime)
+
   
-    # Storing all the primes generated in a
-    # an array primes[]
     primes = [0 for i in range(2,n+1)]
     j = 0
     for p in range(2,n+1):
        if (isPrime[p]):
            primes[j] = p
-           j += 1
-  
-    # Printing all those prime numbers that
-    # occupy prime numbered position in
-    # sequence of prime numbers.
+   
+         
     for k in range(j):
         if (isPrime[k+1]):
             print (primes[k],end=" ")
